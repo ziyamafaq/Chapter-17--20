e1edab8e34f6d70d23bd6fd9ceb4d4d0528bed9b
ziyamafaq <142810274+ziyamafaq@users.noreply.github.com>
2023-09-18 17:50:57 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,70 @@
+// *******************************************HOME ASSIGNMENT************************************************** 
+
+// *******************Q1**********************
+// for (var i = 0; i <= 10; i++) {
+//   console.log("welcome user", i);
+// }
+
+// *******************Q2**********************
+// for (var i = 0; i <= 12; i++){
+//     console.log(i , "Ali")
+// }
+
+// *******************Q3**********************
+// for(var i=0;i<=4;i++){
+//     console.log(i)
+// }
+
+// // *******************Q4**********************
+// for (var a = 0; a < 101; a++)
+// console.log(a)
+
+// // *******************Q5**********************
+// for (var i = 0; i >3; i++){
+//     console.log().,,
+// }
+
+
+
+// // *******************Q6**********************
+// var arr = [];
+
+// arr[0] = prompt("Enter first value");
+// arr[1] = prompt("Enter second value");
+// arr[2] = prompt("Enter third value");
+// arr[3] = prompt("Enter fourth value");
+// arr[4] = prompt("Enter fifth value");
+// arr[5] = prompt("Enter sixth value");
+
+// console.log(arr);
+
+
+// // *******************Q7**********************
+// var flag = []
+// flag[4] = prompt("Enter your value")
+// console.log(flag);
+
+// // *******************Q8**********************
+// for (var i = 0; i <= 1; i++){
+//     console.log(i, "Welcome")
+// }
+
+// // *******************Q9**********************
+// for (var i = 0; i <=10; i++){
+//      console.log(i)
+//      alert(counter)
+// }
+
+// // *******************Q10**********************
+// var arr = prompt("Enter you name")
+// for (var i = 0; i <= 5 ;)
+//    if( i === ziyam){
+//   alert("Enter");
+// } else {
+//      alert("Please write correct user name");
+// }
+
+
+// for (var i = 0; i <=6; i++)
+// var firstArr = [a, b, c, d, e, f];
+// var secondArr = [1, 2,3, 4, 5, 6];
